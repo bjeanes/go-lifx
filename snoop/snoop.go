3fea9ba7947ca9a4b15d495e97f45a253d75cf13
Bo Jeanes <me@bjeanes.com>
2014-04-01 11:36:40 -0700

    Move current main to a separate 'snoop' command

---
+++ snoop/snoop.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bjeanes/go-lifx/protocol"
+)
+
+func main() {
+	conn, err := protocol.Connect()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	msgs, errs := protocol.NewMessageDecoder(conn.Datagrams)
+
+	for {
+		select {
+		case msg := <-msgs:
+			fmt.Printf("%v\n", &msg)
+		case err := <-errs:
+			switch e := err.(type) {
+			case protocol.BadDatagram:
+				fmt.Printf("Error (%s) decoding datagram: %+v", e.Error(), e.Datagram)
+			default:
+				// ignore
+			}
+		}
+	}
+}
