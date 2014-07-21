d23e34adddbc21e5f35e24196b9dbfaf588a8562
Bo Jeanes <me@bjeanes.com>
2014-07-20 20:08:54 -0700

    Pretty print bytes in lifx-snoop

--- lifx-snoop.go
+++ lifx-snoop.go
@@ -6,6 +6,21 @@ import (
 	"github.com/fatih/color"
 )
 
+// Print byte array as a grid of hex numbers
+func printBytes(bytes []byte) {
+	out := color.New(color.FgWhite)
+	out.Printf("DATA: length=%d (%04x)", len(bytes), len(bytes))
+	for i, b := range bytes {
+		if i%16 == 0 {
+			out.Println()
+			out.Print("      ")
+		}
+
+		out.Printf("%02x ", b)
+	}
+	out.Println()
+}
+
 func main() {
 	conn, err := protocol.Connect()
 
@@ -30,16 +45,17 @@ func main() {
 
 	for {
 		b := <-bytes
+		printBytes(b)
+		color.Set(color.Bold)
 		select {
 		case msg := <-msgs:
-			out := color.New(color.FgGreen)
-			out.Printf("DATA: %v\n", b)
-			out.Add(color.Bold).Printf("MSG:  %v", &msg)
+			color.Set(color.FgGreen)
+			fmt.Printf("MSG:  %v", &msg)
 		case err := <-errs:
-			out := color.New(color.FgRed)
-			out.Printf("DATA: %v\n", b)
-			out.Add(color.Bold).Printf("ERR:  %s", err.Error())
+			color.Set(color.FgRed)
+			fmt.Printf("ERR:  %s", err.Error())
 		}
+		color.Unset()
 		fmt.Printf("\n\n")
 	}
 }
