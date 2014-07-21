3910d84971d1c1b5f0dc1a98d93c0dee7c9a2103
Bo Jeanes <me@bjeanes.com>
2014-07-20 20:41:52 -0700

    Use hexdump-like output in lifx-snoop

--- lifx-snoop.go
+++ lifx-snoop.go
@@ -1,25 +1,12 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/bjeanes/go-lifx/protocol"
 	"github.com/fatih/color"
+	"regexp"
 )
-
-// Print byte array as a grid of hex numbers
-func printBytes(bytes []byte) {
-	out := color.New(color.FgWhite)
-	out.Printf("DATA: length=%d (%04x)", len(bytes), len(bytes))
-	for i, b := range bytes {
-		if i%16 == 0 {
-			out.Println()
-			out.Print("      ")
-		}
-
-		out.Printf("%02x ", b)
-	}
-	out.Println()
-}
 
 func main() {
 	conn, err := protocol.Connect()
@@ -43,19 +30,24 @@ func main() {
 
 	msgs, errs := protocol.NewMessageDecoder(datagrams)
 
+	re := regexp.MustCompilePOSIX("^00000")
+
 	for {
 		b := <-bytes
-		printBytes(b)
+
+		out := color.New(color.FgWhite)
+		out.Printf("DATA: length=%d\n", len(b))
+		out.Print(re.ReplaceAllString(hex.Dump(b), "      "))
 		color.Set(color.Bold)
+
 		select {
 		case msg := <-msgs:
-			color.Set(color.FgGreen)
-			fmt.Printf("MSG:  %v", &msg)
+			out.Add(color.FgGreen)
+			out.Printf("MSG:  %v", &msg)
 		case err := <-errs:
-			color.Set(color.FgRed)
-			fmt.Printf("ERR:  %s", err.Error())
+			out.Add(color.FgRed)
+			out.Printf("ERR:  %s", err.Error())
 		}
-		color.Unset()
 		fmt.Printf("\n\n")
 	}
 }
