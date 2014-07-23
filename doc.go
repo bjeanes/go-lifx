82fe5b64dbecc0d967dd84f97356e304fbfc639f
Bo Jeanes <me@bjeanes.com>
2014-07-22 23:22:24 -0700

    Add drafty top-level package doc

---
+++ doc.go
@@ -0,0 +1,16 @@
+/*
+A set of libraries and commands for working with LIFX's wifi-enabled light bulbs.
+
+See the following sub-packages for more details:
+
+• Protocol - Encoding/decoding of LIFX messages and transmission/reception
+of such.
+
+• Client - Higher-level abstractions ("light bulb", "label", "colors",
+etc.) and actions ("set label", "set lights to a specific colour", etc.).
+
+• CLI Tools - These are foremost examples of using the Client and/or
+Protocol components but also serve as debugging tools and command line
+control over bulbs (e.g. from Bash scripts).
+*/
+package lifx
