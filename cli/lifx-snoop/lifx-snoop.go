5d2897226ff591d8d01dd47241a8d5963c89fd2a
Bo Jeanes <me@bjeanes.com>
2014-07-29 09:25:40 -0700

    Implement basic payload type filtering in lifx-snoop

--- cli/lifx-snoop/lifx-snoop.go
+++ cli/lifx-snoop/lifx-snoop.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"github.com/bjeanes/go-lifx/protocol"
 	"github.com/fatih/color"
+	"os"
 	"regexp"
 	"strings"
 )
 
 func main() {
+	filter := ""
+
+	if len(os.Args) >= 2 {
+		filter = strings.ToLower(os.Args[1])
+	}
+
 	conn, err := protocol.Connect()
 
 	if err != nil {
@@ -31,26 +38,36 @@ func main() {
 
 	msgs, errs := protocol.NewMessageDecoder(decoderDatagrams)
 
-	re := regexp.MustCompilePOSIX("^00000")
-
 	for {
 		d := <-snoopDatagrams
 
 		out := color.New(color.FgWhite)
-		out.Printf("FROM: %s\n", d.From)
-		out.Printf("SIZE: %d\n", len(d.Data))
-		data := strings.TrimLeft(hex.Dump(d.Data), "0")
-		out.Printf("DATA: 000" + re.ReplaceAllString(data, "      "))
-		color.Set(color.Bold)
 
 		select {
 		case msg := <-msgs:
+			t := strings.ToLower(fmt.Sprintf("%T", msg.Payload))
+			if !strings.Contains(t, filter) {
+				break
+			}
+			printDatagram(out, &d)
 			out.Add(color.FgGreen)
 			out.Printf("MSG:  %+v\n      %T %+v", msg.Header, msg.Payload, msg.Payload)
+			fmt.Printf("\n\n")
 		case err := <-errs:
+			printDatagram(out, &d)
 			out.Add(color.FgRed)
 			out.Printf("ERR:  %s", err.Error())
+			fmt.Printf("\n\n")
 		}
-		fmt.Printf("\n\n")
 	}
+}
+
+func printDatagram(out *color.Color, d *protocol.Datagram) {
+	re := regexp.MustCompilePOSIX("^00000")
+
+	out.Printf("FROM: %s\n", d.From)
+	out.Printf("SIZE: %d\n", len(d.Data))
+	data := strings.TrimLeft(hex.Dump(d.Data), "0")
+	out.Printf("DATA: 000" + re.ReplaceAllString(data, "      "))
+	color.Set(color.Bold)
 }
